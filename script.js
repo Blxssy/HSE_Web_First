a5ac9e82de2a59d4ec6d6505984c3793501af7d5
blxsyy <roma.asav@gmail.com>
2025-03-09 19:55:25 +0300

    fix image

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function toggleInfo() {
 	var facts = document.getElementById('facts')
+	var button = document.getElementById('toggleButton')
 	if (facts.classList.contains('hidden')) {
 		facts.classList.remove('hidden')
+		button.textContent = 'Скрыть'
 	} else {
 		facts.classList.add('hidden')
+		button.textContent = 'Показать интересные факты'
 	}
 }
